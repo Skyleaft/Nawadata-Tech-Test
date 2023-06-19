674a219db88704cf6ec7145683f43e019dbad8d6
Skyleaft <milzan_malik@outlook.com>
2023-06-19 11:31:20 +0700

    init

---
+++ PSBB/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int totalBus = 0;
+        int busLimit = 4;
+
+        Console.Write("Input the number of families :");
+        int totalFamily = int.Parse(Console.ReadLine());
+        Console.Write("Input the number of members in the family (separated by a space) :");
+        List<int> familymember = Console.ReadLine().Split().Select(int.Parse).ToList();
+        if (familymember.Count != totalFamily)
+        {
+            Console.WriteLine($"Input must be equal with count of family");
+            return;
+        }
+
+        double member = (double)familymember.Sum() / busLimit;
+        totalBus = (int)Math.Ceiling(member);
+        Console.WriteLine($"Minimun Bus Required : {totalBus}");
+        Console.ReadLine();
+    }
+}
---
+++ SortVowelConsonant/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.Write("Input one line of words(S): ");
+        string str = Console.ReadLine();
+        str = str.Replace(" ", string.Empty);
+        List<string> vowelList = new List<string>();
+        List<string> consonanList = new List<string>();
+        char[] vowelCharacter = { 'a', 'i', 'u', 'e', 'o' };
+
+        var distinc = str.ToLower().Distinct().ToList();
+
+        foreach (var item in distinc)
+        {
+            var vowel = vowelCharacter.Where(x => x == item).FirstOrDefault();
+            var res = str.Where(x => x == vowel).ToList();
+            if (res.Count > 0)
+            {
+                res.ForEach(x => vowelList.Add(x.ToString()));
+            }
+            else
+            {
+                consonanList.Add(item.ToString());
+            }
+
+        }
+
+        Console.WriteLine("Vowel Character : ");
+        vowelList.ForEach(x => Console.Write(x));
+        Console.WriteLine();
+        Console.WriteLine("Consonan Character :");
+        consonanList.ForEach(x => Console.Write(x));
+        Console.ReadLine();
+    }
+}
